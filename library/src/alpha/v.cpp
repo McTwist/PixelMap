230f8e476ba4343011e11da496a9c24bd205f315
McTwist <rajb89@hotmail.com>
2024-11-10 10:22:45 +0100

    Deduplicate alpha v.cpp

--- library/src/alpha/v.cpp
+++ library/src/alpha/v.cpp
@@ -5,6 +5,21 @@
 #include "util/nibble.hpp"
 #include "util/palette.hpp"
 
+#include <spdlog/spdlog.h>
+
+#include <functional>
+
+
+template<typename T>
+void convert_alpha([[maybe_unused]] const NBT::NBTByteArray & src, std::vector<T> & dst, std::size_t n, const std::function<T(T, std::size_t)> & convert)
+{
+	assert(src.size() == dst.size());
+	for (auto x = 0; x < SECTION_X; ++x)
+		for (auto y = 0; y < SECTION_Y; ++y)
+			for (auto z = 0; z < SECTION_Z; ++z)
+				dst[(y << 8) | (z << 4) | x] = convert(dst[(y << 8) | (z << 4) | x], (x << 11) | (z << 7) | (y + (n << 4)));
+}
+
 
 alpha::V::V(Chunk & _chunk)
 	: chunk(_chunk)
@@ -39,58 +54,54 @@ bool alpha::V::visit(const NBT::Tag & tag)
 		else if (tag.isName("Blocks"))
 		{
 			auto source = tag.get<NBT::NBTByteArray>();
-			for (auto n = 0; n < 8; ++n)
+			for (std::size_t n = 0; n < 8; ++n)
 			{
 				auto & b = blocks[n];
 				if (b.empty())
 					b.resize(SECTION_SIZE);
-				for (auto x = 0; x < SECTION_X; ++x)
-					for (auto y = 0; y < SECTION_Y; ++y)
-						for (auto z = 0; z < SECTION_Z; ++z)
-							b[(y << 8) | (z << 4) | x] = (b[(y << 8) | (z << 4) | x] & 0xFF00) | uint16_t(source[(x << 11) | (z << 7) | (y + (n << 4))]);
+				convert_alpha<uint16_t>(source, b, n, [&source](uint16_t d, std::size_t i) -> uint16_t {
+					return (d & 0xFF00) | uint16_t(source[i]);
+				});
 			}
 		}
 		else if (tag.isName("Data"))
 		{
 			auto source = tag.get<NBT::NBTByteArray>();
-			for (auto n = 0; n < 8; ++n)
+			for (std::size_t n = 0; n < 8; ++n)
 			{
 				auto & b = blocks[n];
 				if (b.empty())
 					b.resize(SECTION_SIZE);
-				for (auto x = 0; x < SECTION_X; ++x)
-					for (auto y = 0; y < SECTION_Y; ++y)
-						for (auto z = 0; z < SECTION_Z; ++z)
-							b[(y << 8) | (z << 4) | x] = (b[(y << 8) | (z << 4) | x] & 0x0FFF) | uint16_t(nibble4(source, (x << 11) | (z << 7) | (y + (n << 4))) << 12);
+				convert_alpha<uint16_t>(source, b, n, [&source](uint16_t d, std::size_t i) -> uint16_t {
+					return (d & 0x0FFF) | uint16_t(nibble4(source, i) << 12);
+				});
 			}
 		}
 		else if (tag.isName("BlockLight"))
 		{
 			auto source = tag.get<NBT::NBTByteArray>();
-			for (auto n = 0; n < 8; ++n)
+			for (std::size_t n = 0; n < 8; ++n)
 			{
 				auto & section = sections[n];
 				std::vector<uint8_t> b;
 				b.resize(SECTION_SIZE);
-				for (auto x = 0; x < SECTION_X; ++x)
-					for (auto y = 0; y < SECTION_Y; ++y)
-						for (auto z = 0; z < SECTION_Z; ++z)
-							b[(y << 8) | (z << 4) | x] = nibble4(source, (x << 11) | (z << 7) | (y + (n << 4)));
+				convert_alpha<uint8_t>(source, b, n, [&source](uint8_t, std::size_t i) -> uint8_t {
+					return nibble4(source, i);
+				});
 				section.setBlockLight(b);
 			}
 		}
 		else if (tag.isName("SkyLight"))
 		{
 			auto source = tag.get<NBT::NBTByteArray>();
-			for (auto n = 0; n < 8; ++n)
+			for (std::size_t n = 0; n < 8; ++n)
 			{
 				auto & section = sections[n];
 				std::vector<uint8_t> b;
 				b.resize(SECTION_SIZE);
-				for (auto x = 0; x < SECTION_X; ++x)
-					for (auto y = 0; y < SECTION_Y; ++y)
-						for (auto z = 0; z < SECTION_Z; ++z)
-							b[(y << 8) | (z << 4) | x] = nibble4(source, (x << 11) | (z << 7) | (y + (n << 4)));
+				convert_alpha<uint8_t>(source, b, n, [&source](uint8_t, std::size_t i) -> uint8_t {
+					return nibble4(source, i);
+				});
 				section.setSkyLight(b);
 			}
 		}
