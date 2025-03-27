908fca690ed21fc17e1416a89cc122c7e5c052b8
McTwist <rajb89@hotmail.com>
2025-03-27 11:22:40 +0100

    Fix definition error

--- library/tests/color-print.hpp
+++ library/tests/color-print.hpp
@@ -5,9 +5,16 @@
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/gtx/io.hpp>
 
+namespace glm
+{
+
 // Overload to display values correctly for char
 template<typename CTy, typename CTr>
-std::basic_ostream<CTy,CTr>& glm::operator<<(std::basic_ostream<CTy,CTr>& out, utility::RGBA const& g)
+std::basic_ostream<CTy,CTr>& operator<<(std::basic_ostream<CTy,CTr>& out, utility::RGBA const& g);
+template<typename CTy, typename CTr>
+std::basic_ostream<CTy,CTr>& operator<<(std::basic_ostream<CTy,CTr>& out, utility::RGBA const& g)
 {
-	return out << glm::i16vec4(g);
+	return out << i16vec4(g);
+}
+
 }
