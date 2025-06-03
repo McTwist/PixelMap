958a0187863bdb8e08ccb44deac2567cd7491836
McTwist <rajb89@hotmail.com>
2025-06-03 12:44:19 +0200

    Replace patching with external fork

--- cli/cmake/dependencies.cmake
+++ cli/cmake/dependencies.cmake
@@ -1,13 +1,13 @@
 
 message(STATUS "Preparing dependencies for cli...")
 
-set(SPDMON_VERSION 9ca6d4325c70d39da67867aae13f3729b942a000) # Has no tags
+set(SPDMON_VERSION 2e139eb0159b84aa7f47c8c714ba2e62f6bf2169) # Has no tags
 
 # spdmon
 if(PIXELMAPCLI_USE_SPDMON)
 	FetchContent_Declare(
 		SPDMON
-		GIT_REPOSITORY "https://github.com/pfmephisto/spdmon.git"
+		GIT_REPOSITORY "https://github.com/McTwist/spdmon.git"
 		GIT_TAG "${SPDMON_VERSION}"
 		EXCLUDE_FROM_ALL
 		CONFIGURE_COMMAND ""
@@ -17,13 +17,6 @@ if(PIXELMAPCLI_USE_SPDMON)
 	FetchContent_Populate(SPDMON)
 	# Note: CMakelists.txt is broken, set everything manually
 	set(SPDMON_DIR "${spdmon_SOURCE_DIR}")
-	# Patch bug in spdmon
-	execute_process(
-		COMMAND git reset --hard
-		WORKING_DIRECTORY "${SPDMON_DIR}")
-	execute_process(
-		COMMAND git apply "${CMAKE_CURRENT_LIST_DIR}/spdmon.patch"
-		WORKING_DIRECTORY "${SPDMON_DIR}")
 	get_filename_component(SPDMON_INCLUDE_DIR "${SPDMON_DIR}/include" ABSOLUTE CACHE)
 	set(SPDMON_INCLUDE_DIRS "${SPDMON_INCLUDE_DIR}" CACHE PATH "spdmon include dirs" FORCE)
 	set(SPDMON_LICENSE_FILE "${spdmon_SOURCE_DIR}/LICENSE")
